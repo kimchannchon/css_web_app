87ef320159f791a42ff5492e2b3592a0393836de
root <kimchannchon101@gmail.com>
2022-10-22 16:11:37 +0700

    navbar header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export const Header = (props) => {
         <div className='overlay'>
           <div className='container'>
             <div className='row'>
-              <div className='col-md-8 col-md-offset-2 intro-text'>
+              <div className='col-lg-8 intro-text'>
                 <h1>
                   {props.data ? props.data.title : 'Loading'}
                   <span></span>
@@ -15,7 +15,7 @@ export const Header = (props) => {
                   href='#features'
                   className='btn btn-custom btn-lg page-scroll'
                 >
-                  Learn More
+                  ▼
                 </a>{' '}
               </div>
             </div>
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,6 +1,6 @@
 export const Navigation = (props) => {
   return (
-    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top shadow'>
       <div className='container'>
         <div className='navbar-header'>
           <button
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-            React Landing Page
+            LOGO
           </a>{' '}
         </div>
 
@@ -24,7 +24,7 @@ export const Navigation = (props) => {
           className='collapse navbar-collapse'
           id='bs-example-navbar-collapse-1'
         >
-          <ul className='nav navbar-nav navbar-right'>
+          <ul className='nav navbar-nav navbar-left'>
             <li>
               <a href='#features' className='page-scroll'>
                 Features
